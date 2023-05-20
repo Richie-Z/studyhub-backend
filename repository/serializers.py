7e2f1fadb22a976bdb48cd7daddad70aa928f4a1
Richie-Z <zakariarichie@yahoo.com>
2023-05-20 15:27:49 +0700

    use serializers instead of forms

--- repository/serializers.py
+++ repository/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from authentication.models import User
+
 from .models import Repository, RepositoryStar
 
 
@@ -25,3 +27,28 @@ class RepositorySerializer(serializers.ModelSerializer):
         if user and user.is_authenticated:
             return RepositoryStar.objects.filter(repository=obj, user=user).exists()
         return False
+
+    def save(self, **kwargs):
+        repository_name = self.validated_data.get("repository_name")
+        repository_detail = self.validated_data.get("repository_detail")
+        user = kwargs.get("user")
+        repository = Repository.objects.create_repository(
+            repository_name=repository_name,
+            repository_detail=repository_detail,
+            user=user,
+        )
+
+        return repository
+
+
+class RepositoryStarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RepositoryStar
+        fields = ["user", "repository"]
+        extra_kwargs = {"user": {"required": False}}
+
+    def save(self, **kwargs):
+        repository = self.validated_data["repository"]
+        user = kwargs.get("user")
+        repo = RepositoryStar.objects.toggle(user=user, repository=repository)
+        return repo
--- repository/views.py
+++ repository/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 
 from project.helpers import create_response
 from repository.models import Repository, RepositoryStar
-from repository.serializers import RepositorySerializer
+from repository.serializers import RepositorySerializer, RepositoryStarSerializer
 
 from .forms import RepositoryForm, RepositoryStarForm
 
@@ -13,51 +13,44 @@ from .forms import RepositoryForm, RepositoryStarForm
 @permission_classes([IsAuthenticated])
 class CreateRepositoryView(APIView):
     def post(self, request):
-        form = RepositoryForm(request.POST)
-        if form.is_valid():
-            repository_name = form.cleaned_data["repository_name"]
-            repository_detail = form.cleaned_data["repository_detail"]
-            user = form.cleaned_data["user"]
-            repository = Repository.objects.create_repository(
-                repository_name=repository_name,
-                repository_detail=repository_detail,
-                user=user,
-            )
+        serializer = RepositorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
             return create_response("Success Create Repository", status.HTTP_201_CREATED)
         else:
             return create_response(
                 "Error Create Repository",
                 status.HTTP_400_BAD_REQUEST,
-                {"errors": form.errors},
+                {"errors": serializer.errors},
             )
 
 
 @permission_classes([IsAuthenticated])
 class RepositoryStarView(APIView):
     def post(self, request):
-        user = request.user
-        form = RepositoryStarForm(request.POST)
-        if form.is_valid():
-            repository = form.cleaned_data["repository"]
-            RepositoryStar.objects.toggle(user=user, repository=repository)
+        serializer = RepositoryStarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
             return create_response("Success Toggle star Repository", status.HTTP_200_OK)
         else:
             return create_response(
                 "Error Toggle Repository",
                 status.HTTP_400_BAD_REQUEST,
-                {"errors": form.errors},
+                {"errors": serializer.errors},
             )
 
 
 @permission_classes([IsAuthenticated])
 class GetAllRepository(APIView):
     def get(self, request, *args, **kwargs):
-        user = self.kwargs.get("user") or request.user
+        user = kwargs.get("user") or request.user
         if user == "is_null":
             return create_response("Invalid User ID", status.HTTP_404_NOT_FOUND)
         try:
-            repository = Repository.objects.get(user=user)
-            serializer = RepositorySerializer(repository, context={"request": request})
+            repository = Repository.objects.filter(user=user)
+            serializer = RepositorySerializer(
+                repository, many=True, context={"request": request}
+            )
             data = serializer.data
             return create_response("Get Data Success", status.HTTP_200_OK, data)
         except Repository.DoesNotExist:

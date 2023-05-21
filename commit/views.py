ce4ad036666678ac85ae4905c2291e8b3f183c47
Richie-Z <zakariarichie@yahoo.com>
2023-05-21 17:01:04 +0700

    validation if else more readable

--- commit/views.py
+++ commit/views.py
@@ -4,7 +4,6 @@ from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-from commit.models import CommitFile
 from commit.serializers import CommitSerializer, FileSerializer
 from project.helpers import create_response
 
@@ -14,23 +13,23 @@ from project.helpers import create_response
 class CreateCommitFileView(APIView):
     def post(self, request):
         commit_serializer = CommitSerializer(data=request.data)
-        if commit_serializer.is_valid():
-            commit = commit_serializer.save(user=request.user)
-            commit_file_serializer = FileSerializer(
-                data=request.data, context={"commit": commit}
-            )
-            if commit_file_serializer.is_valid():
-                commit_file_serializer.save(commit=commit)
-                return create_response("Success Create Commit", status.HTTP_201_CREATED)
-            else:
-                return create_response(
-                    "Error Create Commit (File)",
-                    status.HTTP_400_BAD_REQUEST,
-                    {"errors": commit_file_serializer.errors},
-                )
-        else:
+        if not commit_serializer.is_valid():
             return create_response(
                 "Error Create Commit",
                 status.HTTP_400_BAD_REQUEST,
                 {"errors": commit_serializer.errors},
             )
+
+        commit = commit_serializer.save(user=request.user)
+
+        commit_file_serializer = FileSerializer(
+            data=request.data, context={"commit": commit}
+        )
+        if not commit_file_serializer.is_valid():
+            return create_response(
+                "Error Create Commit (File)",
+                status.HTTP_400_BAD_REQUEST,
+                {"errors": commit_file_serializer.errors},
+            )
+        commit_file_serializer.save(commit=commit)
+        return create_response("Success Create Commit", status.HTTP_201_CREATED)
